a4fa601458a2c90eb5a647ab4c2715bf8d1b067d
Alexandra Pominova <145675215+alfoll@users.noreply.github.com>
2026-01-29 15:41:21 +0300

    Initial commit: Spring Boot backend setup

---
+++ src/main/kotlin/com/cleaningapp/backend/CleaningAppBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.cleaningapp.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CleaningAppBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<CleaningAppBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/cleaningapp/backend/CleaningAppBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cleaningapp.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CleaningAppBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
