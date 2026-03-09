ede652eddf63f518c10f3112affb0734979c7f74
Alexandra Pominova <145675215+alfoll@users.noreply.github.com>
2026-03-09 23:35:31 +0300

    В TaskRepository.kt методы теперь сортируют на уровне бд ответы (сортировка с последнего)

--- src/main/kotlin/com/cleaningapp/backend/task/TaskRepository.kt
+++ src/main/kotlin/com/cleaningapp/backend/task/TaskRepository.kt
@@ -3,26 +3,27 @@ package com.cleaningapp.backend.task
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
+// добавить сортировки???
 interface TaskRepository: JpaRepository<TaskEntity, UUID> {
-    // задачи хозяйства
-    fun findAllByHouseholdId(householdId: UUID): List<TaskEntity>
+    // задачи хозяйства (сорт по созданию сначалп последние)
+    fun findAllByHouseholdIdOrderByCreatedAtDesc(householdId: UUID): List<TaskEntity>
 
-    // выполненные и не выполненные задачи хозяйства
-    fun findAllByHouseholdIdAndIsCompletedFalse(householdId: UUID): List<TaskEntity>
-    fun findAllByHouseholdIdAndIsCompletedTrue(householdId: UUID): List<TaskEntity>
+    // выполненные и не выполненные задачи хозяйства (сорт по выполнению - сначала последние)
+    fun findAllByHouseholdIdAndIsCompletedFalseOrderByCompletedAtDesc(householdId: UUID): List<TaskEntity>
+    fun findAllByHouseholdIdAndIsCompletedTrueOrderByCompletedAtDesc(householdId: UUID): List<TaskEntity>
 
-    // чтобы освобождать забронированные невыполненные задачи
+    // чтобы освобождать забронированные невыполненные задачи (сорт не нужен - внуренний метод)
     // МОИ забронированные и не выполненные - зачем если есть следующий метод? - для освобождения по всем хозяйствам
     fun findAllByAssignedToIdAndIsCompletedFalse(assignedToId: UUID): List<TaskEntity>
 
-    // МОИ забронированные задачи в хозяйстве
-    fun findAllByHouseholdIdAndAssignedToIdAndIsCompletedFalse(
+    // МОИ забронированные задачи в хозяйстве (сорт по брони - сначала последние)
+    fun findAllByHouseholdIdAndAssignedToIdAndIsCompletedFalseOrderByAssignedAtDesc(
         householdId: UUID,
         assignedToId: UUID
     ): List<TaskEntity>
 
-    // не забронированные и не выполненные - свободные задачи
-    fun findAllByHouseholdIdAndAssignedToIsNullAndIsCompletedFalse(
+    // не забронированные и не выполненные - свободные задачи (сорт по созданию - сначала последние)
+    fun findAllByHouseholdIdAndAssignedToIsNullAndIsCompletedFalseOrderByCreatedAtDesc(
         householdId: UUID
     ): List<TaskEntity>
 }
